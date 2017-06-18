799910e6f9681bb077354c508a4e7ed73c2401df
vittal <vitshacov@gmail.com>
2017-06-18 21:40:46 +0300

    add routes

--- app/index.js
+++ app/index.js
@@ -1,7 +1,25 @@
 window.jQuery = require('jquery');
 import './style.css'
 import angular from 'angular';
+import {ngRoute} from 'angular-route';
 import './navbar/navbar.module';
 import './schedule/schedule.module';
 import './mainItem/mainItem.module';
-const app = angular.module('app', ['navbar','schedule', 'mainItem'])
+import homeRoute from  './routes/home.html'
+const app = angular.module('app', ['ngRoute','navbar','schedule', 'mainItem']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        template : homeRoute
+    })
+    .when("/london", {
+        template : "london.htm",
+    })
+    .when("/paris", {
+        template : "paris.htm",
+    })
+    .otherwise({
+        template: homeRoute
+    });
+});
