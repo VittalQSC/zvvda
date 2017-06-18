18780eabff4a3bab19fbdcd848bdae878c30ab4c
vittal <vitshacov@gmail.com>
2017-06-18 23:10:31 +0300

    add hideSchedule btn icon behavior

--- app/schedule/schedule.component.js
+++ app/schedule/schedule.component.js
@@ -4,9 +4,11 @@ export default {
   controller: function () {
     this.scheduleName = "schedule vittal";
     this.hideSchedule = false;
+    this.arrowBtnMode = !this.hideSchedule ? "up" : "down"; 
     this.toggleArrowBtn = (e) => {
       e.preventDefault();
       this.hideSchedule = !this.hideSchedule;
+      this.arrowBtnMode = !this.hideSchedule ? "up" : "down"; 
       // debugger;
       // console.log(e);
     }
