80c677fcc5405dd627b88164aeaff6bd6ed8ddf4
VitaliShatsou <Vitali.Shatsou@thomsonreuters.com>
2017-07-09 15:57:46 +0300

    add createTournamentPage

--- app/admin/createTournamentPage/createTournamentPage.component.js
+++ app/admin/createTournamentPage/createTournamentPage.component.js
@@ -3,13 +3,34 @@ import createTournamentPageTemplate from './createTournamentPage.template.html'
 export default {
     template: createTournamentPageTemplate,
     controller: function ($http) {
-      this.user = 'VITTAL';
-      $http.post('http://localhost:8081/tournaments/link',{
-          "link": "http://chessarbiter.com/turnieje_warcaby/2016/f_4621/"
-      }).then(res => {alert(res)}, e=>{console.log('err', e);});
-      
-      $http.get('localhost:8081/countries/cities')
-      .then(res => {console.log(res)}, ()=>{console.log('err');});
+      this.data = {
+        arbiter: null,
+        begin: null,
+        city: {
+          country: null,
+          id: null,
+          name: null,
+        },
+        currentRound: null,
+        end: null,
+        externalUrl: null,
+        id: null,
+        name: null,
+        organizer: null,
+        roundAmount: null,
+        shortName: null,
+        system: null
+      };
+
+      this.importClick = () => {
+        const url = this.data.externalUrl;
+        $http.post('http://localhost:8081/tournaments/link',{"link": url}).then(res => {
+          Object.assign(this.data, res.data);
+        }, e=>{
+          console.log('err', e);
+        });
+      }
+
     }
   }
 
