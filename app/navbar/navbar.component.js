8d880659584aa6da90923009b2b06f74f3b0b938
VitaliShatsou <Vitali.Shatsou@thomsonreuters.com>
2017-07-09 18:46:23 +0300

    small fixes

--- app/navbar/navbar.component.js
+++ app/navbar/navbar.component.js
@@ -3,7 +3,9 @@ import navbarTemplate from './navbar.template.html'
 export default {
     template: navbarTemplate,
     controller: function () {
-      this.user = 'VITTAL';
+      this.title = 'EYC Draughts 2017';
+      this.results = 'Results';
+      this.regulations = 'Regulations';
     }
   }
 
