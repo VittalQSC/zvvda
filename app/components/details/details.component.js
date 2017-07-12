d78d678fc2df95502716be39e03f15480d2cb464
VitaliShatsou <Vitali.Shatsou@thomsonreuters.com>
2017-07-12 19:00:51 +0300

    add more localization for results

--- app/components/details/details.component.js
+++ app/components/details/details.component.js
@@ -1,38 +1,24 @@
 import angular from 'angular';
 import detailsTemplate from './details.template.html';
-import { configs } from './../../utils/';
+import { configs, translationManager } from './../../utils/';
 export default {
     template: detailsTemplate,
     controller: function ($routeParams, $http) {
-      console.log('$routeParams', $routeParams);
+     Object.assign(this, {
+        Tournament_details: "Tournament_details",
+        Place: "Place",
+        Players_number: "Players_number",
+        Current_Round: "Current_Round",
+        Arbiter: "Arbiter",
+        Days: "Days",
+      });
+
       this.tournamentData = {};
       $http.get(`http://${configs.host}:${configs.port}/tournaments/${$routeParams.tournamentId}`)
       .then(res => {
         console.log('suc', res);
         this.tournamentData = Object.assign({}, res.data);
       }, err=>{console.log(err)});
-
-    
-      // const mockRes = {
-      //     "id": null,
-      //     "name": "European Championship Hopes Boys 2016 (B10) Blitz",
-      //     "shortName": null,
-      //     "begin": "2016-08-09",
-      //     "end": "2016-08-09",
-      //     "arbiter": "Vital Aniśka",
-      //     "organizer": null,
-      //     "city": {
-      //         "id": null,
-      //         "name": "Pinsk/EDC/BDF",
-      //         "country": null
-      //     },
-      //     "system": "SWISS",
-      //     "roundAmount": 9,
-      //     "currentRound": 9,
-      //     "externalUrl": "http://chessarbiter.com/turnieje_warcaby/2016/f_4621/"
-      // };     
-
-      // this.tournamentData = Object.assign({}, mockRes); 
     }
   }
 
--- app/components/details/details.module.js
+++ app/components/details/details.module.js
@@ -1,7 +1,10 @@
 import angular from 'angular'
 import detailsComponent from './details.component'
-
+import { translationManager } from './../../utils/';
 const details = angular.module('details', [])
+              .config(function ($translateProvider) {  
+                translationManager.translateProviderInit($translateProvider);              
+              })
               .component('details2', detailsComponent)
               .name;
 
