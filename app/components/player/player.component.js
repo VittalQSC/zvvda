77ce3f6bf3fd7554f1551651063015587ed419ce
VittalQSC <vitshacov@gmail.com>
2017-07-16 01:50:53 +0300

    add players

--- app/components/player/player.component.js
+++ app/components/player/player.component.js
@@ -1,6 +1,6 @@
 import angular from 'angular'
 import playerTemplate from './player.template.html'
-import { configs, translationManager } from './../../utils/';
+import { configs, constants, translationManager } from './../../utils/';
 export default {
     template: playerTemplate,
     bindings: {
@@ -10,12 +10,16 @@ export default {
       translationManager.subscribe($translate);
       this.player = {};
       this.tournaments = [{}];
+      this.teamClassifications = [];
+      this.countryCode = "";
       this.$onInit = function() {
         // console.log(this.playerId);
         $http.get(`http://${configs.host}:${configs.port}/players/${this.playerId}`).then(res => {
           this.player = res.data;
           console.log(res);
           this.tournaments = this.player.tournaments;
+          this.teamClassifications = this.player.teamClassifications;
+          this.countryCode = constants.mapCountryToCountryCode[this.player.country.name];
         });
                   
       }
