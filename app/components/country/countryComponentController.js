a8fc14d91a32131196bdea48f2cc34adea832ecc
VittalQSC <vitshacov@gmail.com>
2017-08-03 11:32:34 +0300

    add countries

--- app/components/country/countryComponentController.js
+++ app/components/country/countryComponentController.js
@@ -3,9 +3,18 @@ import { translationManager, configs, constants } from './../../utils/';
 let CountryComponentController = function ($routeParams, $translate, $http) {
     translationManager.subscribe($translate);
     const countryId = $routeParams.id;
+    this.mapCountryToCountryCode = constants.mapCountryToCountryCode;
+    $http.get(`http://${configs.host}:${configs.port}/countries`)
+    .then(res => {
+      let countries = res.data;
+      let index = countries.map(c => "" + c.id).indexOf(countryId);
+      this.country = countries[index];
+    });
     $http.get(`http://${configs.host}:${configs.port}/countries/${countryId}/players`)
     .then(res => {
       console.log('country info', res.data);
+      this.results = res.data;
+
     });
   }
 
